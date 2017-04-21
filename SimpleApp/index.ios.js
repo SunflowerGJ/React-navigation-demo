211ddc38b2cca01b1009b8952c084eb2a257c13a
GJ <gaojie603@sina.com>
2017-04-21 11:51:15 +0800

    提交到分支测试1

--- SimpleApp/index.ios.js
+++ SimpleApp/index.ios.js
@@ -31,7 +31,7 @@ class RootScreen extends React.Component {
              <Text>Hello, Navigation!</Text>
              <Button
               onPress={() => navigate('Home',{user:'yingying'})}
-              title="Chat with Lucy，hhei"
+              title="Chat with Lucy，hh"
             />
               <Button
                   onPress={() => navigate('DrawerNavRoot',{user:'gaojie'})}
